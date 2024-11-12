5c1ddb0623c3e87783cda86220cab69418137fce
Ujjwal Sonawat <ujjwalsonawat7@gmail.com>
2024-11-12 18:37:37 +0530

    completed

--- src/Canvas.jsx
+++ src/Canvas.jsx
@@ -1,36 +1,65 @@
-import React, { useEffect, useRef, useState } from "react";
-import images from "./image";
+import { useEffect, useRef, useState } from "react";
+import canvasImages from "./canvasimages";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
-const Canvas = ({ details }) => {
-  const scale = window.devicePixelRatio
+
+function Canvas({ details }) {
   const { startIndex, numImages, duration, size, top, left, zIndex } = details;
-  const [index, setIndex] = useState({ value: details.startIndex })
+
+  const [index, setIndex] = useState({ value: startIndex });
   const canvasRef = useRef(null);
+
   useGSAP(() => {
     gsap.to(index, {
-      value: details.startIndex + details.numImages - 1, repeat: -1, duration: details.duration, ease: "linear", onUpdate: () => {
-        setIndex({ value: Math.round(index.value) })
-      }
+      value: startIndex + numImages - 1,
+      duration: duration,
+      repeat: -1,
+      ease: "linear",
+      onUpdate: () => {
+        setIndex({ value: Math.round(index.value) });
+      },
     });
-  }, [index]);
+
+    gsap.from(canvasRef.current, {
+      opacity: 0,
+      duration: 1,
+      ease: "power2.inOut",
+    });
+  });
+
   useEffect(() => {
+    const scale = window.devicePixelRatio;
     const canvas = canvasRef.current;
     const ctx = canvas.getContext("2d");
     const img = new Image();
-    img.src = images[index.value];
+    img.src = canvasImages[index.value];
     img.onload = () => {
       canvas.width = canvas.offsetWidth * scale;
       canvas.height = canvas.offsetHeight * scale;
       canvas.style.width = canvas.offsetWidth + "px";
-      canvas.style.height = canvas.offsetHeight + "px ";
+      canvas.style.height = canvas.offsetHeight + "px";
+
       ctx.scale(scale, scale);
       ctx.drawImage(img, 0, 0, canvas.offsetWidth, canvas.offsetHeight);
     };
-  });
-  return <canvas data-scroll data-scroll-speed={
-    Math.random().toFixed(1)
-  } id="canvas" className="absolute" style={{ width: `${size * 1.8}px`, height: `${size * 1.8}px`, top: `${top}%`, left: `${left}%`, zIndex: `${zIndex}` }} ref={canvasRef}></canvas>;
-};
+  }, [index]);
+
+  return (
+    <canvas
+      data-scroll
+      data-scroll-speed={Math.random().toFixed(1)}
+      ref={canvasRef}
+      className="absolute"
+      style={{
+        width: `${size * 1.8}px`,
+        height: `${size * 1.8}px`,
+        top: `${top}%`,
+        left: `${left}%`,
+        zIndex: `${zIndex}`,
+      }}
+      id="canvas"
+    ></canvas>
+  );
+}
 
 export default Canvas;
